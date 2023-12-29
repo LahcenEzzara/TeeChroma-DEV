01bbbaf037cfb6e80337bb5b888e88097bef9819
Lahcen Ezzara <114707306+LahcenEzzara@users.noreply.github.com>
2023-12-29 22:35:03 +0100

    Static Files

---
+++ src/components/Index.jsx
@@ -0,0 +1,20 @@
+import React, { useEffect, useState } from "react";
+
+function Index() {
+  let [htmlFileString, setHtmlFileString] = useState();
+
+  async function fetchHtml() {
+    setHtmlFileString(await (await fetch(`../static/index.html`)).text());
+  }
+  useEffect(() => {
+    fetchHtml();
+  }, []);
+
+  return (
+    <div>
+      <div dangerouslySetInnerHTML={{ __html: htmlFileString }}></div>
+    </div>
+  );
+}
+
+export default Index;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -9,53 +9,58 @@ import {
 } from "../config/motion";
 import state from "../store";
 import CustomButton from "../components/CustomButton";
+import Index from "../components/Index.jsx";
 
 const Home = () => {
   const snap = useSnapshot(state);
 
   return (
-    <AnimatePresence>
-      {snap.intro && (
-        <motion.section className="home" {...slideAnimation("left")}>
-          <motion.header {...slideAnimation("down")}>
-            <img
-              src="./logo.png"
-              alt="logo"
-              className="w-8 h-8 object-contain"
-            />
-          </motion.header>
-          <motion.div className="home-content" {...headContainerAnimation}>
-            <motion.div {...headTextAnimation}>
-            <h1 className="head-text">
-             LET'S <br className="xl:block hidden" /> DO IT.
-            </h1>
+    <>
+      <AnimatePresence>
+        {snap.intro && (
+          <motion.section className="home" {...slideAnimation("left")}>
+            <motion.header {...slideAnimation("down")}>
+              <img
+                src="./logo.png"
+                alt="logo"
+                className="w-8 h-8 object-contain"
+              />
+            </motion.header>
+            <motion.div className="home-content" {...headContainerAnimation}>
+              <motion.div {...headTextAnimation}>
+                <h1 className="head-text">
+                  LET'S <br className="xl:block hidden" /> DO IT.
+                </h1>
 
+                <motion.div
+                  {...headContentAnimation}
+                  className="flex flex-col gap-5"
+                >
+                  <p className="max-w-md font-normal text-gray-600 text-base">
+                    Elevate your style game with TeeChroma for the Morocco World
+                    Cup 2030! Personalize your jersey in 3D and let your
+                    creativity run wild.
+                    <br />
+                    <strong>
+                      Because your passion deserves a unique look!
+                    </strong>
+                  </p>
 
-
-
-              <motion.div
-                {...headContentAnimation}
-                className="flex flex-col gap-5"
-              >
-                <p className="max-w-md font-normal text-gray-600 text-base">
-                  Elevate your style game with TeeChroma for the Morocco World Cup 2030!
-                  Personalize your jersey in 3D and let your creativity run wild.<br/>
-                  <strong>Because your passion deserves a unique look!</strong> 
-                </p>
-
-
-                <CustomButton
-                  type="filled"
-                  title="Unleash Your Creativity"
-                  handleClick={() => (state.intro = false)}
-                  customStyles="w-fit px-4 py-2.5 font-bold text-sm"
-                />
+                  <CustomButton
+                    type="filled"
+                    title="Unleash Your Creativity"
+                    handleClick={() => (state.intro = false)}
+                    customStyles="w-fit px-4 py-2.5 font-bold text-sm"
+                  />
+                </motion.div>
               </motion.div>
             </motion.div>
-          </motion.div>
-        </motion.section>
-      )}
-    </AnimatePresence>
+          </motion.section>
+        )}
+      </AnimatePresence>
+
+      <Index />
+    </>
   );
 };
 
